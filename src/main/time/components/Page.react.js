d5cf256fb804760ff78db37a668e2b278e6d4a7d
mepheser <matthias.epheser@indoqa.com>
2016-04-11 11:30:45 +0200

    enhance doc links in README

--- src/main/time/components/Page.react.js
+++ src/main/time/components/Page.react.js
@@ -16,5 +16,5 @@ class TimePage extends React.Component {
 
 export default TimePage
 
-// Note: top level route components NEED to be react classes and can't be written as functional components!!
+// Note: top level route components NEED to be react classes and can't be written as functional components!
 // Otherwise hot reloading won't work: https://github.com/gaearon/react-hot-loader/issues/212
