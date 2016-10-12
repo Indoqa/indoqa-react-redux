1a8d7f2c3f6ea81942e7c116c721baeba1ccafb0
mepheser <matthias.epheser@indoqa.com>
2016-10-12 15:47:37 +0200

    auto-generate demo app in /docs

---
+++ indoqa-webpack-docs.js
@@ -0,0 +1,9 @@
+module.exports = {
+  appName: 'indoqa-react-redux-docs',
+  mainJs: './src/main/index.js',
+  outputPath: './docs',
+  outputPublicPath: '/indoqa-react-redux',
+  createIndexHtml: true,
+  isReactLibrary: false,
+  externals: {}
+}
