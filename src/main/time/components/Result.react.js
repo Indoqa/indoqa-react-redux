7b13c5bdbc88ce1115ed87fb9644a8dce528d770
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-04-03 07:54:19 +0200

    formatting

--- src/main/time/components/Result.react.js
+++ src/main/time/components/Result.react.js
@@ -1,32 +1,44 @@
 // @flow
 import React from 'react'
 
-import {Result} from '../types/Result'
+import type {Result} from '../types/Result'
 
 type Props = {
   result: Result,
   error: string,
   isLoading: boolean
-};
+}
 
 export default ({result, error, isLoading}: Props) => {
   if (isLoading) {
-    return <span> loading... </span>
+    return (
+      <span>
+        loading...
+      </span>
+    )
   }
 
   if (error) {
-    return <span> ERROR fetching time: {error} </span>
+    return (
+      <span>
+        ERROR fetching time: {error}
+      </span>
+    )
   }
 
   if (!result) {
-    return <span> Select a location! </span>
+    return (
+      <span>
+        Select a location!
+      </span>
+    )
   }
 
   return (
     <div>
-      <div> current time: {result.time} </div>
-      <div> country: {result.countryName} </div>
-      <div> timezone: {result.timezoneId} </div>
+      <div>current time: {result.time}</div>
+      <div>country: {result.countryName}</div>
+      <div>timezone: {result.timezoneId}</div>
     </div>
   )
 }
