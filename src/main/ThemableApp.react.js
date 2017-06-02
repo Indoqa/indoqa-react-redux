b2433245493c068571ffbeb6e84854c680a0bd5d
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-06-02 18:13:25 +0200

    Themable app

---
+++ src/main/ThemableApp.react.js
@@ -0,0 +1,31 @@
+import React, {PropTypes, Component} from 'react'
+import {ThemeProvider} from 'react-fela'
+import merge from 'deepmerge'
+
+import {theme as baseTheme} from './fela/theme'
+import theme from './theme.js'
+
+const buildTheme = (customTheme) => {
+  return merge(baseTheme, customTheme)
+}
+
+class ThemableApp extends Component {
+
+  render() {
+    return (
+      <ThemeProvider theme={buildTheme(theme)}>
+        {this.props.children}
+      </ThemeProvider>
+    )
+  }
+}
+
+ThemableApp.propTypes = {
+  children: PropTypes.element,
+}
+
+ThemableApp.defaultProps = {
+  children: null,
+}
+
+export default ThemableApp
--- src/main/routes.react.js
+++ src/main/routes.react.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {IndexRoute, Route} from 'react-router'
 
+import ThemableApp from './ThemableApp.react.js'
 import TimePage from './time/components/TimePage.react.js'
-import TodosPage from './todos/components/TodosPage.react'
+import TodosPage from './todos/components/TodosPage.react.js'
 
 const routes = (
-  <Route path="/">
+  <Route component={ThemableApp} path="/">
     <IndexRoute component={TimePage} />
     <Route component={TodosPage} path="/todos" />
   </Route>
