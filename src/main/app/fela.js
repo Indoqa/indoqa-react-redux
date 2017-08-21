46700044ff39bd52fa2de8766bea41758068c551
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-08-21 17:56:52 +0200

    upgrade indoqa-react-app / indoqa-react-fela

--- src/main/app/fela.js
+++ src/main/app/fela.js
@@ -1,7 +1,28 @@
+import presetWeb from 'fela-preset-web'
+import namedMediaQuery from 'fela-plugin-named-media-query'
+import monolithic from 'fela-monolithic'
+
 const init = (renderer) => {
   renderer.renderStatic('html, body, #app {height: 100%}')
 }
 
+const namedMediaQueryPlugin = namedMediaQuery({
+  desktop: '@media (min-width: 1024px)',
+  tablet: '@media (min-width: 768px)',
+})
+
+const config = {
+  plugins: [
+    ...presetWeb,
+    namedMediaQueryPlugin,
+  ],
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  config.enhancers = [monolithic()]
+}
+
 export default {
   init,
+  config,
 }
