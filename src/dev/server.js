4be9e46ebca924e455ff75003981bfa959ad0001
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-10-11 20:10:03 +0200

    reenable hot reload

--- src/dev/server.js
+++ src/dev/server.js
@@ -4,15 +4,10 @@ const runDevServer = require('indoqa-webpack').runDevServer
 
 const routesCallback = ({app, proxy}) => {
   app.use('/geonames', proxy('http://api.geonames.org', {
-    forwardPath: (req) => {
+    proxyReqPathResolver: (req) => {
       // modify url path if needed
       return url.parse(req.url).path
     },
-    decorateRequest: (req) => {
-      // add Authorization if needed
-      // req.headers['Authorization'] = 'Basic 12345='
-      return req
-    }
   }))
 }
 
