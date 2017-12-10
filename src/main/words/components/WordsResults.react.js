5f385b41cc4c33c26009d8c310d1e2cffe0a4220
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-12-10 19:30:28 +0100

    words: poweredBy Wordnik

--- src/main/words/components/WordsResults.react.js
+++ src/main/words/components/WordsResults.react.js
@@ -8,16 +8,23 @@ type Props = {
 
 const renderWords = (results: Array<string>) => {
   return results.map((word) => (
-    <li key={word}>{word}</li>
+    <li key={word}><a href={`http://www.wordnik.com/words/${word}`} target="_blank">{word}</a></li>
   ))
 }
 
 const WordsResults = ({results}: Props) => {
+  const poweredBy = (
+    <Box mt={3}>
+      <img alt="powered by Wordnik" src="http://www.wordnik.com/img/wordnik_badge_a2.png" />
+    </Box>
+  )
+
   return (
     <Box mt={1}>
       <ul>
         {renderWords(results)}
       </ul>
+      {results.length > 0 && poweredBy}
     </Box>
   )
 }
