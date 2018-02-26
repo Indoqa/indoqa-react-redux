e4c16ce7bf4836e87d6e4fd25108fedfcf7cf583
Reinhard Pötz <reinhard.poetz@indoqa.com>
2018-02-26 17:52:47 +0100

    user addresses subforms

--- src/main/forms/components/UserForm.react.js
+++ src/main/forms/components/UserForm.react.js
@@ -1,42 +1,70 @@
 // @flow
 import * as React from 'react'
-import {Form, Field, Formik} from 'formik'
+import {Form, Field, FieldArray, Formik} from 'formik'
 import {createComponent} from 'react-fela'
 import yup from 'yup'
 
+type Props = {
+  user: any,
+}
+
 const UserLabel = createComponent(() => ({
-  display: 'block',
-  width: '300',
+  display: 'inline-block',
+  width: '100',
 }), 'label')
+
+const Row = createComponent(() => ({
+  marginTop: 3,
+  marginBottom: 3,
+}))
 
 const validationSchema = () => {
   return yup.object().shape({
     name: yup.string().required(),
     email: yup.string().required('E-mail is required!!!'),
+    addresses: yup.array().of(yup.object().shape({
+      country: yup.string().required(),
+    })),
   })
 }
 
-const user = {
-  id: '',
-  name: 'Maier',
-  email: 'w.maier@example.com',
-  addresses: [
-    {
-      addressLine: 'Schottenring 3',
-      city: 'Vienna',
-      zipCode: '1010',
-      country: 'Austria',
-    },
-    {
-      addressLine: 'Heinrichstrasse 7',
-      city: 'Graz',
-      zipCode: '8010',
-      country: 'Austria',
-    }
-  ],
+const EMPTY_ADDRESS = {
+  street: '',
+  city: '',
+  zipCode: '',
+  country: '',
 }
 
-const UserForm = () => {
+const renderAddressForm = (arrayHelpers, address, index) => {
+  return (
+    <div key={index}>
+      <Row>
+        <UserLabel>Street</UserLabel>
+        <Field name={`addresses.${index}.street`} />
+      </Row>
+      <Row>
+        <UserLabel>City</UserLabel>
+        <Field name={`addresses.${index}.city`} />
+      </Row>
+      <Row>
+        <UserLabel>Zip code</UserLabel>
+        <Field name={`addresses.${index}.zipCode`} />
+      </Row>
+      <Row>
+        <UserLabel>Country</UserLabel>
+        <Field name={`addresses.${index}.country`} />
+      </Row>
+      <button
+        type="button"
+        onClick={() => arrayHelpers.remove(index)}
+      >
+        -
+      </button>
+    </div>
+  )
+}
+
+const UserForm = ({user}:Props) => {
   return (
     <Formik
       displayName="UserForm"
@@ -45,9 +73,9 @@ const UserForm = () => {
       validateOnBlur={false}
       validateOnChange={false}
       validationSchema={validationSchema}
-      render={() => (
+      render={({values}) => (
         <Form>
-          <div>
+          <Row>
             <UserLabel>Name</UserLabel>
             <Field
               name="name"
@@ -55,9 +83,8 @@ const UserForm = () => {
                 <input {...field} />
               )}
             />
-          </div>
-          <br />
-          <div>
+          </Row>
+          <Row>
             <UserLabel>E-Mail</UserLabel>
             <Field
               name="email"
@@ -65,8 +92,28 @@ const UserForm = () => {
                 <input {...field} />
               )}
             />
-          </div>
-          <br />
+          </Row>
+
+          <FieldArray
+            name="addresses"
+            render={arrayHelpers => (
+              <div>
+                <div>Addresses</div>
+                {values.addresses && values.addresses.length > 0 ? (
+                  values.addresses.map((address, index) => (
+                    renderAddressForm(arrayHelpers, address, index)
+                  )))
+                  : null
+                }
+                <button
+                  type="button"
+                  onClick={() => arrayHelpers.push(EMPTY_ADDRESS)}
+                >
+                  Add address
+                </button>
+              </div>
+            )}
+          />
           <button type="submit">Submit</button>
         </Form>
       )}
--- src/main/forms/components/UserPage.react.js
+++ src/main/forms/components/UserPage.react.js
@@ -7,13 +7,33 @@ import UserForm from './UserForm.react.js'
 
 type Props = {}
 
+const user = {
+  id: '',
+  name: 'Maier',
+  email: 'w.maier@example.com',
+  addresses: [
+    {
+      street: 'Schottenring 3',
+      city: 'Vienna',
+      zipCode: '1010',
+      country: 'Austria',
+    },
+    {
+      street: 'Heinrichstrasse 7',
+      city: 'Graz',
+      zipCode: '8010',
+      country: 'Austria',
+    }
+  ],
+}
+
 class UserPage extends React.Component<Props> {
 
   render() {
     return (
       <MainMenuTemplate title="Add a user">
         <Box m={1}>
-          <UserForm />
+          <UserForm user={user} />
         </Box>
       </MainMenuTemplate>
     )
