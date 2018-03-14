1c88738eb86de4c16a3e0326517a9dff183f4631
Reinhard Pötz <reinhard.poetz@indoqa.com>
2018-03-14 20:08:58 +0100

    router: use combineReducersWithRouter helper function

--- src/main/app/rootReducer.js
+++ src/main/app/rootReducer.js
@@ -1,6 +1,5 @@
 // @flow
-import {combineReducers} from 'redux'
-import {routerReducer} from 'react-router-redux'
+import {combineReducersWithRouter} from 'indoqa-react-app'
 
 import forms from '../forms/store/forms.reducer'
 import time from '../time/store/time.reducer'
@@ -8,11 +7,10 @@ import todos from '../todos/store/todos.reducer'
 import words from '../words/store/words.reducer'
 
 const reducers = {
-  routing: routerReducer,
   forms,
   time,
   todos,
   words,
 }
 
-export default combineReducers(reducers)
+export default combineReducersWithRouter(reducers)
