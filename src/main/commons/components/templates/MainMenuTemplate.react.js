0aa0c2e611e62e576135daabf407012821f35884
Reinhard Pötz <reinhard.poetz@indoqa.com>
2018-06-09 19:39:38 +0200

    slide menu

--- src/main/commons/components/templates/MainMenuTemplate.react.js
+++ src/main/commons/components/templates/MainMenuTemplate.react.js
@@ -77,11 +77,14 @@ const FixedBar = createComponentWithProxy(() => ({
   width: '100%',
 }), Box)
 
-const MobileMenu = createComponentWithProxy(({theme}) => ({
-  position: 'fixed',
-  height: `calc(100% - ${theme.layout.actionBarHeight}px)`,
-  top: theme.layout.actionBarHeight,
+const MobileMenu = createComponentWithProxy(({theme, show}) => ({
+  position: 'absolute',
   zIndex: 10,
+  top: theme.layout.actionBarHeight,
+  left: show ? 0 : theme.layout.menuWidth * -1,
+  height: `calc(100% - ${theme.layout.actionBarHeight}px)`,
+  width: theme.layout.menuWidth,
+  transition: 'left 0.15s',
   ...MOBILE_ONLY,
 }), Box)
 
@@ -126,33 +129,20 @@ class MainMenuTemplate extends React.Component<Props, State> {
     this.setState({showMobileMenu: !this.state.showMobileMenu})
   }
 
-  renderMenuIcon() {
-    return <MenuIcon onClick={() => this.toggleMenu()}>[Menu]</MenuIcon>
-  }
-
-  renderMobileMenu() {
-    if (this.state.showMobileMenu) {
-      return (
-        <MobileMenu>
-          <MainMenu />
-        </MobileMenu>
-      )
-    }
-    return null
-  }
-
   render() {
     const {title, header, children} = this.props
     const documentTitle = title === undefined ? BASE_TITLE : `${BASE_TITLE} | ${title}`
     return (
       <Flex stretch height="100%">
         <DocumentTitle title={documentTitle} />
-        {this.renderMobileMenu()}
+        <MobileMenu show={this.state.showMobileMenu}>
+          <MainMenu />
+        </MobileMenu>
         <ContentOverlay show={this.state.showMobileMenu} onClick={() => this.toggleMenu()} />
         <Box grow={1}>
           <FixedBar>
             <Bar pl={2} pr={2}>
-              {this.renderMenuIcon()}
+              <MenuIcon onClick={() => this.toggleMenu()}>[Menu]</MenuIcon>
               {renderHeaderContent(title, header)}
               {renderLanguageSwitcher()}
             </Bar>
